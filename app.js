2a6b0f0789bb3264926a329489ddabad2060eda9
formesean <seanaguilar698@gmail.com>
2024-09-14 20:48:20 +0800

    [feature] first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Git in Sync!");
+});
+
+const server = app.listen(8080, () => {
+  console.log("Listening to port 8080.");
+});
